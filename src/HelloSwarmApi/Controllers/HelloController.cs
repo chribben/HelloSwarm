2e1d70695c69154641bcbf23091622d8e2388be8
Christian Jacobsen <chribben@gmail.com>
2016-06-28 09:24:10 +0200

    Add source

---
+++ src/HelloSwarmApi/Controllers/HelloController.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloMvcApi
+{
+    [Route("/api/hello")]
+    public class HelloController
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new OkObjectResult("Hello brave new Docker!");
+        }
+    }
+}
